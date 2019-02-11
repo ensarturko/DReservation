1c0d6dab9ec26d71202dbce58cedd6e48fb6f896
Ensar Topbas <ensardev@gmail.com>
2019-02-11 14:28:03 +0300

    As a quick solution, hardly coded the facilityId which was not specified in the case instruction page.

--- DReservation/Models/Domain/Reservation.cs
+++ DReservation/Models/Domain/Reservation.cs
@@ -4,7 +4,7 @@ namespace DReservation.Models.Domain
 {
     public class Reservation
     {
-        public Guid FacilityId { get; set; } = new Guid();
+        public Guid FacilityId { get; set; } = new Guid("b04f80b4-5c35-42f3-941c-6c78a726387b");
 
         public DateTime Start { get; set; }
 
